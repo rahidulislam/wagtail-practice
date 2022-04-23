bf05fadce2a2bd64ca7e7f57a7d06e1d4a4fa4c2
Rahidul Islam <rahiseli@outlook.com>
2022-04-23 20:11:33 +0600

    Initialize Project

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-7eje@#@0d=43sx_!2f+x#i!6bix3hgvf6#4g(48yt&b@ls45^o'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
